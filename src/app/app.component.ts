1f2a63b36ace065c9665699002a9b704c1d08342
ilyas-utor <ilyas@utorsols.com>
2022-04-29 16:17:28 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wikipedia-search-app';
+  searchTerm: any;
+  results:any = []; // empty array
+
+  page: number = 1;
+  totalResults: any = []; // total count of search result will be pushed here 
+
+  constructor(private wiki: WikiService){}
+
+  onSubmit(){
+    this.wiki.search(this.searchTerm).subscribe( (res:any) =>{
+     this.results = res.query.search;
+     this.totalResults = res.query.search.length;
+     console.log("Total Search Items: ", this.totalResults);
+     console.log(this.results);
+    })
+  }
+
+
 }
